afe356669dbf69a1d05842d245cca58e133ff694
YP-PY <39273667+YP-PY@users.noreply.github.com>
2020-12-28 21:58:02 +0100

    Update router.sh

--- router.sh
+++ router.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #update the iptables
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
 sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
